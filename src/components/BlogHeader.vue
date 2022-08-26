148abe5454530eae36aa28d554791e5e147c6ab1
jiwon <owohjw@gmail.com>
2022-08-26 09:42:18 +0900

    최초프로젝트구성

---
+++ src/components/BlogHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <header class="header">
+    <h1>Header</h1>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+  h1 {
+    font-weight: 700;
+    text-align: center;
+  }
+</style>>
